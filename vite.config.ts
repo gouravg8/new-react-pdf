d701532bd9931b3ff64178e07a53b4bdf8056ec2
gourav <gouravsoni8476@gmail.com>
2025-04-01 12:23:24 +0530

    Bump version to 0.0.4, update build script order, add TypeScript declaration generation, and configure Vite for library mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.tsx"),
+      name: "MyComponent",
+      fileName: (format) => `my-component.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+    },
+  },
 });
